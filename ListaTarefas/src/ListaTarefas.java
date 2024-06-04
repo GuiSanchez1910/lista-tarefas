a657e5b477f402d5690c7bab49bb34adf8a8929a
Auto Logon <guilherme.sanchez1910@gmail.com>
2024-06-04 19:23:47 -0300

    ArrayList

---
+++ ListaTarefas/src/ListaTarefas.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ListaTarefas {
+    
+    private static ArrayList<Tarefa> listaTarefas = new ArrayList<>();
+
+    public static ArrayList<Tarefa> getListaTarefas() {
+
+        return listaTarefas;
+    }
+}
